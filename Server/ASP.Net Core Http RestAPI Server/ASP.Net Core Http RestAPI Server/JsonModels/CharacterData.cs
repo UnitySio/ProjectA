fb710d43ab66d169c867bc87850a3288798eac49
UnitySio <dnwls010728@gmail.com>
2022-07-30 06:17:54 +0900

    Server 분리

--- Server/ASP.Net Core Http RestAPI Server/ASP.Net Core Http RestAPI Server/JsonModels/CharacterData.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.Net_Core_Http_RestAPI_Server.JsonDataModels
-{
-    public class CharacterData
-    {
-        public uint CharacterUniqueID { get; set; }
-        public int CharacterGrade { get; set; }
-        public int CharacterLv { get; set; }
-    }
-}
